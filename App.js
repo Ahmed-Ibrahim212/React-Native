aa803bf543e5650d512f131092a0769af704a1d9
Decagon <Decagon@Decagons-MacBook-Air.local>
2022-09-05 15:40:10 +0100

    First Commit

--- App.js
+++ App.js
@@ -7,25 +7,20 @@
  */
 
 import React from 'react';
-import { Provider as StoreProvider } from 'react-redux';
-import { Store }  from './src/redux/store';
+import {Provider as StoreProvider} from 'react-redux';
+import {Store} from './src/redux/store';
 import DrawerNav from './src/navigation/DrawerNav';
 import AppNavigation from './src/navigation/AppNavigation';
 import TopBarNavigation from './src/navigation/TopBarNavigation';
 
 const App = () => {
-
   return (
-    <StoreProvider store={ Store }>
-      
+    <StoreProvider store={Store}>
       <AppNavigation />
       {/* <TopBarNavigation /> */}
 
       {/* <DrawerNav /> */}
-
     </StoreProvider>
-    
-    
   );
 };
 
